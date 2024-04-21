78575ea5ced8871e3712c59e7ef05f2975187904
walther <waltherpires@hotmail.com>
2024-04-21 16:42:38 -0300

    retornei o site ao normal

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express(); //iniciando o express
 
 //criando a rota inicial
 app.get("/", function (req, res) {
-  res.send("<h1>Modifiquei o site</h1>");
+  res.send("<h1>Bem vindo ao meu site!</h1>");
 });
 
 //rota do cadastro de produtos
