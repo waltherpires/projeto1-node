05b2df37b219475e83be721bca3a09db936058f2
walther <waltherpires@hotmail.com>
2024-04-24 17:47:26 -0300

    criei versao secundaria

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("<h1>Versao inicial</h1>");
+  res.send("<h1>Versao secundaria</h1>");
 });
 
 app.get("/musicas", function (req, res) {
